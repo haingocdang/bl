d8e5532beaef3e3a7b00fcccdc47f07aacea6422
Khanh Phan <khanh.phan@alpaca.vn>
2020-11-11 17:43:42 +0700

    Add AddUserPageUI file

---
+++ src/main/java/pageUIs/alpaca/AddUserPageUI.java
@@ -0,0 +1,11 @@
+package pageUIs.alpaca;
+
+public class AddUserPageUI {
+    public static final String CANCEL_BUTTON="//button[contains(.,'Hủy')]";
+    public static final String CREATE_USER_BUTTON="//button[contains(.,'Tạo')]";
+    public static final String USERNAME_TEXTBOX="//input[@id='inputLoginName']";
+    public static final String NAME_TEXTBOX="//input[@id='inputName']";
+    public static final String EMAIL_TEXTBOX="//input[@id='email']";
+    public static final String PHONE_TEXTBOX="//input[@id='inputPhone']";
+    public static final String FILTER_SELECT="//label[text()='Công ty']//parent::fieldset//div[text()='Tìm kiếm']";
+}
